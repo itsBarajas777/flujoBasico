51fd73ccae084c2dbc21d2abea35180b243777b9
itsBarajas777 <axelbarajas000@gmail.com>
2026-03-02 17:24:24 -0800

    Mensaje

--- OPP/ogro.py
+++ OPP/ogro.py
@@ -1,9 +1,17 @@
 from enemigo import *
+import random
 
-class ogro(enemigo):
+class ogro(Enemigo):
+
     def __init__(self, puntos_energia=20, ataque=3):
-        super().__init__(tipo_enemigo='Ogro', puntos_energia=puntos_energia, ataque=ataque)
-
-        def habla(self):
-            print("ogro aplastar todo")
-
+        super().__init__(tipo_enemigo='ogro', puntos_energia=puntos_energia, ataque=ataque)
+        
+    def habla(self):
+        print("ogro aplastar todo!!!")
+        
+    def ataque_especial(self):
+        print("ogro ataque especial")
+        funciona_ataque_especial = random.random() < 0.20
+        if funciona_ataque_especial:
+            self.ataque += 4
+            print("ogro enojado y incremento su ataque por 4")
--- OPP/zombie.py
+++ OPP/zombie.py
@@ -1,16 +1,19 @@
 from enemigo import *
+import random
 
-
-class zombie(enemigo):
+class zombie(Enemigo):
     def __init__(self, puntos_energia=10, ataque=1):
-        super().__init__(tipo_enemigo='zombie', puntos_energia=puntos_energia,ataque=ataque)
+        super().__init__(tipo_enemigo='zombie', puntos_energia=puntos_energia, ataque=ataque)
 
+    def habla(self):
+        print("*Hummmm.....*")
 
-    def habla(seif):
-        print("Hummmmm. . .*")
+    def propagar_enfermedad(self):
+        print("El zombie esta tratando de propagar la enfermedad!!")
 
-
-    def propagador_enfermedad(self):
-        print("El zombie esta tratando de propogar la enfermedad")
-
-
+    def ataque_especial(self):
+        print("zombie ataque especial")
+        funciona_ataque_especial = random.random() < 0.50
+        if funciona_ataque_especial:
+            self.ataque += 2
+            print('zombie ha regenerado su energia con 2HP!!!!')
